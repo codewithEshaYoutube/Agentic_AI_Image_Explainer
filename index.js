24aec443257298c77fa97f1a42c014b8d7f71106
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2026-09-18 13:20:06 +0500

    up

--- index.js
+++ index.js
@@ -3,13 +3,25 @@ const moment = require('moment');
 const simpleGit = require('simple-git');
 
 const FILE_PATH = './data.json';
+const DATE = moment().subtract(1, 'd').format();
 
-const DATE = moment().subtract(1,'d').format();
+async function run() {
+    const data = { date: DATE };
 
-const data = {
-    date : DATE
+    try {
+        await jsonfile.writeFile(FILE_PATH, data, { spaces: 2 });
+        console.log('Wrote', FILE_PATH);
+
+        const git = simpleGit();
+        await git.add([FILE_PATH]);
+        await git.commit(DATE, { '--date': DATE });
+        await git.push();
+
+        console.log('Committed and pushed successfully');
+    } catch (err) {
+        console.error('Failed:', err.message);
+        process.exit(1);
+    }
 }
 
-jsonfile.writeFile(FILE_PATH,data,()=>{
-    simpleGit().add([FILE_PATH]).commit(DATE,{'--date' : DATE}).push();
-});
+run();
